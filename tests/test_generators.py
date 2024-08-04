7b270654b99521af8bfda518e124a5b3316faa21
Alyona Sytnik <alyona-sytnik0908@yandex.ru>
2024-08-04 22:56:55 +0200

    Отформатирован код

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,5 +1,7 @@
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 import pytest
+
+from src.generators import (card_number_generator, filter_by_currency,
+                            transaction_descriptions)
 
 
 def test_filter_by_currency_exceptions(transactions):
@@ -8,7 +10,11 @@ def test_filter_by_currency_exceptions(transactions):
     result = filter_by_currency([], "EUR")
     assert result == "Список пустой!"
 
-@pytest.mark.parametrize("transactions, currency, expected", [([], "EUR", "Список пустой!"), ([], "RUB", "Список пустой!")])
+
+@pytest.mark.parametrize(
+    "transactions, currency, expected",
+    [([], "EUR", "Список пустой!"), ([], "RUB", "Список пустой!")],
+)
 def test_filter_by_currency_exceptions(transactions, currency, expected):
     result = filter_by_currency(transactions, currency)
     assert result == expected
@@ -20,14 +26,16 @@ def test_transaction_descriptions(transactions_fixture):
     assert next(result) == "Перевод со счета на счет"
     assert next(result) == "Перевод со счета на счет"
 
+
 def test_transaction_descriptions_empty():
     result = list(transaction_descriptions([]))
     assert result == ["Список пустой!"]
 
+
 def test_card_number_generator():
     result = card_number_generator(1, 5)
-    assert next(result) == '0000 0000 0000 0001'
-    assert next(result) == '0000 0000 0000 0002'
-    assert next(result) == '0000 0000 0000 0003'
-    assert next(result) == '0000 0000 0000 0004'
-    assert next(result) == '0000 0000 0000 0005'
+    assert next(result) == "0000 0000 0000 0001"
+    assert next(result) == "0000 0000 0000 0002"
+    assert next(result) == "0000 0000 0000 0003"
+    assert next(result) == "0000 0000 0000 0004"
+    assert next(result) == "0000 0000 0000 0005"
