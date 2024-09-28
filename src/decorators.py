33f69a2b997db5eabe8d02c78ccf9a2160bb5f25
Alyona Sytnik <alyona-sytnik0908@yandex.ru>
2024-09-28 19:45:17 +0200

    Создан модуль log

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+from datetime import time
+
+def log(filename=None):
+    def my_decorator(func):
+        def wrapper(*args, **kwargs):
+            result = None
+            ret = ""
+            try:
+                time1 = time()
+                result = func(*args, **kwargs)
+                time2 = time()
+                ret += func.__name__ + " " + str(result) + "\n"
+            except Exception as e:
+                ret += func.__name__ + f" error: {e}. Inputs: {args}, {kwargs}\n"
+
+            if filename is None:
+                print(ret)
+            else:
+                with open(filename, 'w') as file:
+                    file.write(ret)
+                    file.close()
+
+            return result
+
+        return wrapper
+
+    return my_decorator
+
+
+@log()  # filename="mylog.txt"
+def my_function(x, y):
+    return x + y
+
+
