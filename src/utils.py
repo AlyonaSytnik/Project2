f2ec93d17ce7178d659f1ff9c444aa575210bc32
Alyona Sytnik <alyona-sytnik0908@yandex.ru>
2025-02-03 21:47:52 +0200

    Создана функция financial_transactions

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import json
+
+
+def financial_transactions(path):
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        with open(path, encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        return {}
+    except json.decoder.JSONDecodeError:
+        return {}
+    return data
+
+
+
+
+
